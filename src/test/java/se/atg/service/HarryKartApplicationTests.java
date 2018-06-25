ab762d8503bb20a9e763acd163b363758a6df85a
AGHP4M <AGHP4M@RD0059740.global.scd.scania.com>
2018-06-25 20:01:20 +0200

    Test Case Error solving

--- src/test/java/se/atg/service/HarryKartApplicationTests.java
+++ src/test/java/se/atg/service/HarryKartApplicationTests.java
@@ -5,8 +5,10 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import se.atg.service.harrykart.HarryKartApp;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = HarryKartApp.class)
 public class HarryKartApplicationTests {
 
 	@Test
